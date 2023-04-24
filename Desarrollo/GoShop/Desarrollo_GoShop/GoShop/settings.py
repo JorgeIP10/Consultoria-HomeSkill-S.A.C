9a77dee9280d50ba9d38eb822ad3c4b64fbed802
Lokitas01 <lukasameed12@gmail.com>
2023-04-24 00:33:13 -0500

    Cambios realizados en el backend: Requisito 1

--- Desarrollo/GoShop/Desarrollo_GoShop/GoShop/settings.py
+++ Desarrollo/GoShop/Desarrollo_GoShop/GoShop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Ventas_Retail'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import json
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+
+with open('GoShop/credenciales.json') as file:
+    data = json.load(file)
+
+EMAIL_HOST_USER = data['email']
+EMAIL_HOST_PASSWORD = data['password']
--- Desarrollo/GoShop/Desarrollo_GoShop/Ventas_Retail/views.py
+++ Desarrollo/GoShop/Desarrollo_GoShop/Ventas_Retail/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.template.loader import get_template
+from django.core.mail import EmailMultiAlternatives
+import GoShop.settings as settings
 
 # Create your views here.
+def signup(request):
+    if request.method == 'GET':
+        if request.user.is_anonymous:
+            return render(request, 'register.html')
+        else:
+            return redirect('shop')
+    else:
+        try:
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'], email=request.POST['email'])
+            email = request.POST['email']
+            template = get_template('register_email.html')
+            content = template.render({'username': request.POST['username']})
+
+            message = EmailMultiAlternatives("¡Bienvenido a GoShop!",
+                                f"Bienvenido, {request.POST['username']}, te has registrado correctamente a nuestra tienda virtual.",
+                                settings.EMAIL_HOST_USER,
+                                [email])
+            message.attach_alternative(content, 'text/html')
+            user.save()
+            login(request, user)
+            message.send()
+            return redirect('shop')
+        except IntegrityError:
+            return render(request, 'register.html', {'username': request.POST['username'], 'error': 'El usuario ya existe.'})
+
+def signin(request):
+    if request.method == 'GET':
+        if request.user.is_anonymous:
+            return render(request, 'login.html')
+        else:
+            return redirect('shop')
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'login.html', {'error': 'Usuario y/o contraseña incorrecto(s).'})
+        else:
+            login(request, user)
+            return redirect('shop')
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('shop')
+
+def shop(request):
+    return render(request, 'shop.html')
