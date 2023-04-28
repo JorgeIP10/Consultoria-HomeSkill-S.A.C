2cc63c3558746967728bb20fc5dedd7d020b549d
Lokitas01 <lukasameed12@gmail.com>
2023-04-28 01:12:44 -0500

    Se terminan los requisitos 2 y 3

--- Desarrollo/GoShop/Desarrollo_GoShop/GoShop/settings.py
+++ Desarrollo/GoShop/Desarrollo_GoShop/GoShop/settings.py
@@ -123,6 +123,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
 import json
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
